e390ba4387a8ac39dd7d1456722cb709f29a3e58
Szymon Dizewski <szymon.dizewski1@gmail.com>
2021-01-26 14:53:43 +0100

    Solved challenges from edabit.com

---
+++ ReverseAndNot/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReverseAndNot
+{
+    class Program
+    {
+        public static string ReverseAndNot(int number)
+        {
+            string output = $"{number}";
+
+            for (int i = output.Length-1; i >= 0; i--)
+            {
+                output += output[i];
+            }
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseAndNot(123));
+            Console.WriteLine(ReverseAndNot(1525));
+            Console.WriteLine(ReverseAndNot(12334567));
+        }
+    }
+}
